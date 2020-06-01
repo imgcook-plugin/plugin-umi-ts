e2fce9b1b42a39693f1650db614256496b04ae62
Xiaoqiang Yang <i@yxq.me>
2020-06-01 14:23:42 +0800

    improvement: test

--- index.js
+++ index.js
@@ -9,27 +9,25 @@
 const pluginHandler = async (options) => {
   let { data, filePath, config } = options;
 
-  if (data.moduleData && data.moduleData.dsl === "react-standard") {
-    data.code.panelDisplay = data.code.panelDisplay.map((file) => {
-      if (/\.jsx$/.test(file.panelName)) {
-        file.panelType = "ts";
-        file.panelName = file.panelName.replace(/\.jsx$/, ".tsx");
-        file.panelValue = file.panelValue.replace(
-          /('\.\/images.+')/g,
-          (match, p1) => `require(${p1})`
-        );
-        file.panelValue = file.panelValue.replace("lines={undefined}", "");
-        file.panelValue = file.panelValue.replace(
-          /.+(\(\\n.+\);\\n).+/,
-          (match, p1) => {
-            return;
-            `import React from 'react';\nimport styles from './style.response';\nexport default () => ${p1}`;
-          }
-        );
-      }
-      return file;
-    });
-  }
+  data.code.panelDisplay = data.code.panelDisplay.map((file) => {
+    if (/\.jsx$/.test(file.panelName)) {
+      file.panelType = "ts";
+      file.panelName = file.panelName.replace(/\.jsx$/, ".tsx");
+      file.panelValue = file.panelValue.replace(
+        /('\.\/images.+')/g,
+        (match, p1) => `require(${p1})`
+      );
+      file.panelValue = file.panelValue.replace("lines={undefined}", "");
+      file.panelValue = file.panelValue.replace(
+        /.+(\(\\n.+\);\\n).+/,
+        (match, p1) => {
+          return;
+          `import React from 'react';\nimport styles from './style.response';\nexport default () => ${p1}`;
+        }
+      );
+    }
+    return file;
+  });
 
   // body...
   return { data, filePath, config };
