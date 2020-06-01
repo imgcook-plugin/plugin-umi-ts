492378fd1eff363f7e6f7168e346857fae5d6c6c
Xiaoqiang Yang <i@yxq.me>
2020-06-01 11:43:46 +0800

    feat: test

--- index.js
+++ index.js
@@ -6,14 +6,25 @@
  * - config: cli config
  */
 
-const pluginHandler = async options => {
-  let { data, filePath, config  } = options;
+const pluginHandler = async (options) => {
+  let { data, filePath, config } = options;
+
+  data.code.panelDisplay = data.code.panelDisplay.map((file) => {
+    if (/\.jsx$/.test(file.panelName)) {
+      file.panelValue = file.panelValue.replace(
+        /('\.\/images.+')/g,
+        (match, p1) => `require(${p1})`
+      );
+    }
+    return file;
+  });
+
   // body...
   return { data, filePath, config };
 };
 
 module.exports = (...args) => {
-  return pluginHandler(...args).catch(err => {
+  return pluginHandler(...args).catch((err) => {
     console.log(err);
   });
 };
