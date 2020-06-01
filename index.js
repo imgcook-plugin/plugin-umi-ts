0b070e8699623b119386ec8b8fa490e26f1c9bab
Xiaoqiang Yang <i@yxq.me>
2020-06-01 15:16:44 +0800

    improvement: update

--- index.js
+++ index.js
@@ -9,25 +9,36 @@
 const pluginHandler = async (options) => {
   let { data, filePath, config } = options;
 
-  data.code.panelDisplay = data.code.panelDisplay.map((file) => {
-    if (/\.jsx$/.test(file.panelName)) {
-      file.panelType = "ts";
-      file.panelName = file.panelName.replace(/\.jsx$/, ".tsx");
-      file.panelValue = file.panelValue.replace(
-        /('\.\/images.+')/g,
-        (match, p1) => `require(${p1})`
-      );
-      file.panelValue = file.panelValue.replace("lines={undefined}", "");
-      file.panelValue = file.panelValue.replace(
-        /.+(\(\\n.+\);\\n).+/,
-        (match, p1) => {
-          return;
-          `import React from 'react';\nimport styles from './style.response';\nexport default () => ${p1}`;
-        }
-      );
-    }
-    return file;
-  });
+  data.code.panelDisplay = data.code.panelDisplay
+    .map((file) => {
+      if (/\.jsx$/.test(file.panelName)) {
+        file.panelType = "ts";
+        file.panelName = file.panelName.replace(/\.jsx$/, ".tsx");
+        file.panelValue = file.panelValue.replace(
+          /('\.\/images.+')/g,
+          (match, p1) => `require(${p1})`
+        );
+        file.panelValue = file.panelValue.replace(/lines={undefined}/g, "");
+        file.panelValue = file.panelValue.replace(
+          /^(.|\n)+(\s\((.|\n)+\)\;)(.|\n)+$/g,
+          (match, p1, p2) => {
+            return `import React from 'react';\nimport styles from './style.response';\n\nexport default () => {\n\treturn${p2}\n};`;
+          }
+        );
+      }
+
+      if (/\.responsive\.js$/.test(file.panelName)) {
+        file.panelType = "ts";
+        file.panelName = file.panelName.replace(/\.js$/, ".ts");
+        file.panelValue = file.panelValue.replace(
+          /((.|\n)+)(\};)/g,
+          (match, p1, p2, p3) =>
+            `import { CSSProperties } from 'react';\n\n${p1}\n} as {\n\t[key:string]: CSSProperties\n};`
+        );
+      }
+      return file;
+    })
+    .filter((file) => !["style.js", "style.less"].includes(file.panelName));
 
   // body...
   return { data, filePath, config };
